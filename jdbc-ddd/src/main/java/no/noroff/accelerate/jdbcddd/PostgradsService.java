5d25d84cbd44b0f32a4f9c02313daf85c19a0219
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-08-09 11:35:28 +0200

    Lesson content JDBC

--- jdbc-ddd/src/main/java/no/noroff/accelerate/jdbcddd/PostgradsService.java
+++ jdbc-ddd/src/main/java/no/noroff/accelerate/jdbcddd/PostgradsService.java
@@ -1,11 +1,14 @@
 package no.noroff.accelerate.jdbcddd;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import no.noroff.accelerate.jdbcddd.dao.PostgradsDAO;
+import no.noroff.accelerate.jdbcddd.dao.Student;
 
 @Component
 public class PostgradsService implements ApplicationRunner {
@@ -19,7 +22,18 @@ public class PostgradsService implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        pgDao.testDatabaseConnection();
+        // pgDao.testDatabaseConnection();
+        // pgDao.printAllStudents();
+        // pgDao.getStudentById(6);
+        // System.out.println(pgDao.getAllStudents());
+        pgDao.printAllStudents();
+        pgDao.insert(getStudent());
+        pgDao.printAllStudents();
+    }
+
+    private Student getStudent() {
+        Random random = new Random();
+        return new Student(random.nextInt(), "Jane Doe", random.nextInt(1, 3));
     }
 
 }
--- jdbc-ddd/src/main/java/no/noroff/accelerate/jdbcddd/dao/PostgradsDAO.java
+++ jdbc-ddd/src/main/java/no/noroff/accelerate/jdbcddd/dao/PostgradsDAO.java
@@ -2,7 +2,11 @@ package no.noroff.accelerate.jdbcddd.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -23,6 +27,71 @@ public class PostgradsDAO {
         } catch (SQLException sqle) {
             sqle.printStackTrace();
         }
+    }
+
+    public void printAllStudents() {
+        String sql = "SELECT * FROM student";
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            System.out.println("\n");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(2));
+            }
+            System.out.println("\n");
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
+
+    public void getStudentById(int id) {
+        String sql = "SELECT * FROM student WHERE stud_id = ?";
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            System.out.println("\n");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(2));
+            }
+            System.out.println("\n");
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
+
+    public List<Student> getAllStudents() {
+        String sql = "SELECT * FROM student";
+        List<Student> result = new ArrayList<Student>();
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Student student = new Student(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                        resultSet.getInt(3));
+                result.add(student);
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        return result;
+    }
+
+    public int insert(Student student) {
+        String sql = "INSERT INTO student VALUES (?, ?, ?)";
+        int effectedRows = 0;
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, student.id());
+            statement.setString(2, student.name());
+            statement.setInt(3, student.supervisorId());
+            effectedRows = statement.executeUpdate();
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        return effectedRows;
     }
 
 }

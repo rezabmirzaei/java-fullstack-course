df1b245297ab1645db5d28ed606c59684b8d7c27
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-08-11 11:40:18 +0200

    Lesson content JPA

--- spring-jpa/src/main/java/no/noroff/accelerate/springjpa/model/Student.java
+++ spring-jpa/src/main/java/no/noroff/accelerate/springjpa/model/Student.java
@@ -1,12 +1,17 @@
 package no.noroff.accelerate.springjpa.model;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -26,5 +31,16 @@ public class Student {
     @Column(name = "stud_type", length = 15)
     @Enumerated(EnumType.STRING)
     private StudentType type;
+
+    @OneToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "supervisor_id")
+    private Professor professor;
+
+    @ManyToMany
+    private Set<Subject> subjects;
 
 }

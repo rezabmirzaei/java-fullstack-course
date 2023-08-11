50a70725f2f1adf89c277ed31c0f3782cd80314b
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-08-11 20:36:25 +0200

    Lesson content JPA - typo

--- spring-jpa/src/main/resources/schema.sql
+++ spring-jpa/src/main/resources/schema.sql
@@ -5,5 +5,5 @@ DROP TABLE IF EXISTS tb_dummy;
 CREATE TABLE tb_dummy (
 	id int4 NOT NULL,
 	code varchar(5) NOT NULL,
-	CONSTRAINT tb_subject_pkey PRIMARY KEY (id)
+	CONSTRAINT tb_dummy_pkey PRIMARY KEY (id)
 );

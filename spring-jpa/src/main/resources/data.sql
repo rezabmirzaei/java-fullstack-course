a5362663e5c25346b56c92915d8dfa218314105c
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-08-11 20:44:46 +0200

    Lesson content JPA - Add more test data to tables

--- spring-jpa/src/main/resources/data.sql
+++ spring-jpa/src/main/resources/data.sql
@@ -1,2 +1,9 @@
-INSERT INTO tb_professor VALUES (1, 'Ola Nordmann');
-INSERT INTO tb_professor VALUES (2, 'Kari Nordmann');
+INSERT INTO tb_professor(prof_id, name) VALUES (1, 'Ola Nordmann');
+INSERT INTO tb_professor(prof_id, name) VALUES (2, 'Kari Nordmann');
+INSERT INTO tb_professor(prof_id, name) VALUES (3, 'Sven Svensson');
+INSERT INTO tb_professor(prof_id, name) VALUES (4, 'Eva Svensson');
+
+INSERT INTO tb_subject(id, code, title, lecturer_id) VALUES (1, 'ONT401', 'Advanced programming', 1);
+INSERT INTO tb_subject(id, code, title, lecturer_id) VALUES (2, 'DL101', 'Applications of Deep Learning', 2);
+INSERT INTO tb_subject(id, code, title, lecturer_id) VALUES (3, 'SEC401', 'Security Governance', 3);
+INSERT INTO tb_subject(id, code, title, lecturer_id) VALUES (4, 'DS401', 'Securing applications', 4);

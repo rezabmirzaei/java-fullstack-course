012a1b45a952128e2c7b0162ddac284051bfea3a
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-08-12 21:26:47 +0200

    Lesson content JPA, v2

---
+++ spring-jpa2/src/main/java/no/noroff/accelerate/springjpa2/SpringJpa2Application.java
@@ -0,0 +1,13 @@
+package no.noroff.accelerate.springjpa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpa2Application.class, args);
+	}
+
+}
---
+++ spring-jpa2/src/test/java/no/noroff/accelerate/springjpa2/SpringJpa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.noroff.accelerate.springjpa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

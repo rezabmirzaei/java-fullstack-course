57e5cd75cd236d33f12578bfd537a7b38301e54a
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-08-15 20:41:06 +0200

    Spring web ++

--- spring-web-demo/src/main/java/no/noroff/accelerate/controller/HttpMethodMappingController.java
+++ spring-web-demo/src/main/java/no/noroff/accelerate/controller/HttpMethodMappingController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "HTTP Mehods", description = "Demo")
 @RestController
 public class HttpMethodMappingController {
 
